e5f668d9b0e7c55c592636fe20f19db308cf4143
edlymulyaandeslin4 <edlymulyaandeslin@gmail.com>
2023-12-16 15:51:12 +0700

    ubah typo judul login

--- resources/views/auth/login/index.blade.php
+++ resources/views/auth/login/index.blade.php
@@ -1,86 +1,86 @@
 <!DOCTYPE html>
 <html lang="en">
 
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Login</title>
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css">
-    <link href="https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css" rel="stylesheet" />
-    <link rel="stylesheet" href="/css/style.css" />
-</head>
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        <title>Login</title>
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css">
+        <link href="https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css" rel="stylesheet" />
+        <link rel="stylesheet" href="/css/style.css" />
+    </head>
 
-<body>
-    <div class="container">
-        <div class="row">
-            <div>
+    <body>
+        <div class="container">
+            <div class="row">
+                <div>
 
-                @if (session()->has('success'))
-                    <div class="alert alert-success" role="alert">
-                        {{ session('success') }}
-                    </div>
-                @endif
+                    @if (session()->has('success'))
+                        <div class="alert alert-success" role="alert">
+                            {{ session('success') }}
+                        </div>
+                    @endif
 
-                @if (session()->has('error'))
-                    <div class="alert alert-danger" role="alert">
-                        {{ session('error') }}
-                    </div>
-                @endif
+                    @if (session()->has('error'))
+                        <div class="alert alert-danger" role="alert">
+                            {{ session('error') }}
+                        </div>
+                    @endif
 
-                <header class="mt-5">
-                    <h2>Progres Pojek<h2>
-                            <hr>
-                </header>
-                <header style="font-weight: bold">Login</header>
-                <form action="/auth/login" method="post">
-                    @csrf
-                    <div class="input-box">
-
-                    </div>
-                    <div class=" input-box mb-2">
-                        <input type="text" class="input-field" placeholder="Username or Email"
-                            @error('email') is-invalid @enderror id="email" name="email"
-                            value="{{ old('email') }}"> <i class="bx bx-user"></i>
-                        @error('email')
-                            <p class="text-danger">
-                                {{ $message }}
-                            </p>
-                        @enderror
+                    <div class="mt-5 mb-3 text-center text-white">
+                        <h2>Progres Projek<h2>
+                                <hr>
+                                <h2 style="font-weight: bold">Login</h2>
                     </div>
 
+                    <form action="/auth/login" method="post">
+                        @csrf
+                        <div class="input-box">
 
-                    <div class="input-box mb-2">
-                        <input type="password" class="input-field" placeholder="Password"
-                            @error('password') is-invalid @enderror('password') name="password" id="password">
-                        <i class="bx bx-lock-alt"></i>
-                        <span class="show-password-icon" onclick="togglePasswordVisibility('password')">
-                            <i class="bx bx-show" id="passwordIcon"></i>
-                        </span>
-                        @error('password')
-                            <p class="text-danger">
-                                {{ $message }}
-                            </p>
-                        @enderror
-                    </div>
+                        </div>
+                        <div class=" input-box mb-2">
+                            <input type="text" class="input-field" placeholder="Username or Email"
+                                @error('email') is-invalid @enderror id="email" name="email"
+                                value="{{ old('email') }}"> <i class="bx bx-user"></i>
+                            @error('email')
+                                <p class="text-danger">
+                                    {{ $message }}
+                                </p>
+                            @enderror
+                        </div>
 
-                    <button type="submit" class="submit" value="Sign In">Login</button>
-                </form>
-                <div class="two-col mt-2 ">
-                    <div class="quest-box">
-                        <span>Don't have an account?
-                            <a href="/auth/register" onclick="register()">Sign Up</a></span>
-                    </div>
-                    <div class="two">
-                        <label><a href="#">Forgot password?</a></label>
+                        <div class="input-box mb-2">
+                            <input type="password" class="input-field" placeholder="Password"
+                                @error('password') is-invalid @enderror('password') name="password" id="password">
+                            <i class="bx bx-lock-alt"></i>
+                            <span class="show-password-icon" onclick="togglePasswordVisibility('password')">
+                                <i class="bx bx-show" id="passwordIcon"></i>
+                            </span>
+                            @error('password')
+                                <p class="text-danger">
+                                    {{ $message }}
+                                </p>
+                            @enderror
+                        </div>
+
+                        <button type="submit" class="submit" value="Sign In">Login</button>
+                    </form>
+                    <div class="two-col mt-2 ">
+                        <div class="quest-box">
+                            <span>Don't have an account?
+                                <a href="/auth/register" onclick="register()">Sign Up</a></span>
+                        </div>
+                        <div class="two">
+                            <label><a href="#">Forgot password?</a></label>
+                        </div>
                     </div>
                 </div>
-            </div>
 
+            </div>
         </div>
-    </div>
-    <script src="/js/script.js"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
-</body>
+        <script src="/js/script.js"></script>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
+    </body>
 
 </html>
